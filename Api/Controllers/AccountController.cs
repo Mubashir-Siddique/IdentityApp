c0316507070d8c8bb4db1e0aca958066fdbc8be3
Mubashir S <Mubashir S@DESKTOP-GHIM8FK>
2023-07-06 02:56:38 +0500

    Added Account Controller and UserView

---
+++ Api/Controllers/AccountController.cs
@@ -0,0 +1,36 @@
+﻿using Api.Models;
+using Api.Services;
+using Api.ViewModels.Account;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly JWTService _jwtService;
+
+        public AccountController(JWTService jwtService,
+            SignInManager<User> signInManager,
+            UserManager<User> userManager)
+        {
+            _jwtService = jwtService;
+        }
+
+        #region Private Helper Methods
+
+        private UserView CreateApplicationUserDto(User user)
+        {
+            return new UserView
+            {
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                JWT = _jwtService.CreateJWT(user),
+            };
+        }
+        #endregion
+    }
+}
---
+++ Api/ViewModels/Account/UserView.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Api.ViewModels.Account
+{
+    public class UserView
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string JWT { get; set; }
+    }
+}
