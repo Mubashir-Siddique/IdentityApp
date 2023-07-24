3a21f31621c12daf14c3cace6d1c1f09f3b6cf2e
Mubashir-Siddique <mubashir.siddique.ubit@gmail.com>
2023-07-24 21:34:49 +0500

    API endpoint has been added for "Register", added relative private methods.

--- Api/Controllers/AccountController.cs
+++ Api/Controllers/AccountController.cs
@@ -4,6 +4,7 @@ using Api.ViewModels.Account;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Threading.Tasks;
 
 namespace Api.Controllers
@@ -40,6 +41,32 @@ namespace Api.Controllers
 
             return CreateApplicationUserDto(user);
         }
+
+        [HttpPost("Register")]
+        public async Task<IActionResult> Register(RegisterView model)
+        {
+            if (await CheckEmailExistsAsync(model.Email))
+            {
+                return BadRequest($"An Existing account is using {model.Email} email address. Please try with another email");
+            }
+
+            var userToAdd = new User
+            {
+                FirstName = model.FirstName.ToLower(),
+                LastName = model.LastName.ToLower(),
+                Email = model.Email.ToLower(),
+                UserName = model.Email.ToLower(),
+                EmailConfirmed = true
+            };
+
+            var result = await _userManager.CreateAsync(userToAdd, model.Password);
+
+            if (!result.Succeeded) return BadRequest(result.Errors);
+
+            return Ok("Your account has been created, Please Login");
+
+        }
+
         #region Private Helper Methods
 
         private UserView CreateApplicationUserDto(User user)
@@ -50,6 +77,11 @@ namespace Api.Controllers
                 LastName = user.LastName,
                 JWT = _jwtService.CreateJWT(user),
             };
+        }
+
+        private async Task<bool> CheckEmailExistsAsync(string email)
+        {
+            return await _userManager.Users.AnyAsync(x=> x.Email == email.ToLower()); 
         }
         #endregion
     }
---
+++ Api/ViewModels/Account/RegisterView.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Api.ViewModels.Account
+{
+    public class RegisterView
+    {
+        [Required]
+        [StringLength(15,MinimumLength = 3,ErrorMessage = "FirstNa  me must be atleast {2} and maximum {1} characters")]
+        public string FirstName { get; set; }
+        [Required]
+        [StringLength(15, MinimumLength = 3, ErrorMessage = "LastName must be atleast {2} and maximum {1} characters")]
+        public string LastName { get; set; }
+        [Required]
+        [RegularExpression("^\\w+@[a-zA-Z_]+?\\.[a-zA-Z]{2,3}$\r\n", ErrorMessage ="Invalid Email Address")]
+        public string Email { get; set; }   // Also UserName
+        [Required]
+        [StringLength(15, MinimumLength = 8, ErrorMessage = "Password must be atleast {2} and maximum {1} characters")]
+        public string Password { get; set; }
+    }
+}
