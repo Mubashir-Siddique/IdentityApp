7ff9e06e01b26c345c82a0bb940c429e61d1e610
Mubashir S <Mubashir S@DESKTOP-GHIM8FK>
2023-07-24 20:16:01 +0500

    API Endpoint added for Login

--- Api/Controllers/AccountController.cs
+++ Api/Controllers/AccountController.cs
@@ -4,6 +4,7 @@ using Api.ViewModels.Account;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace Api.Controllers
 {
@@ -12,14 +13,33 @@ namespace Api.Controllers
     public class AccountController : ControllerBase
     {
         private readonly JWTService _jwtService;
+        private readonly SignInManager<User> _signInManager;
+        private readonly UserManager<User> _userManager;
 
         public AccountController(JWTService jwtService,
             SignInManager<User> signInManager,
             UserManager<User> userManager)
         {
             _jwtService = jwtService;
+            _signInManager = signInManager;
+            _userManager = userManager;
         }
 
+        [HttpPost("Login")]
+        public async Task<ActionResult<UserView>> Login(LoginView model)
+        {
+            var user = await _userManager.FindByNameAsync(model.UserName);
+
+            if (user == null) return Unauthorized("Invalid Username or Password");
+
+            if (user.EmailConfirmed == false) return Unauthorized("Please confirm your Email!");
+
+            var result = await _signInManager.CheckPasswordSignInAsync(user, model.Password, false);
+
+            if (!result.Succeeded) return Unauthorized("Invalid Username or Password");
+
+            return CreateApplicationUserDto(user);
+        }
         #region Private Helper Methods
 
         private UserView CreateApplicationUserDto(User user)
---
+++ Api/ViewModels/Account/LoginView.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Api.ViewModels.Account
+{
+    public class LoginView
+    {
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
